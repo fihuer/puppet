bee867d014e039e99ca73e341e3fc43fc96679d4
Romain FIHUE <romain.fihue@gmail.com>
2015-08-17 10:23:47 +0200

    (PUP-5097) Adding hostname and domainname in trusted facts
    Before this there is no way to get the hostname/domain fact from a trustworthy source.
    
    With this `hostname` and `domainname` are now part of the trusted facts. Both of them are derived
    from the validated certificate name.
    
    It can be used in Hiera as follows :
    ```yaml
    :hierarchy:
      - "%{::trusted.domainname}/%{::trusted.hostname}"
      - "%{::trusted.domainname}"
      - common
    ```

--- spec/unit/context/trusted_information_spec.rb
+++ spec/unit/context/trusted_information_spec.rb
@@ -40,6 +40,8 @@ describe Puppet::Context::TrustedInformation do
         '1.3.6.1.4.1.34380.1.2.1' => 'CSR specific info',
         '1.3.6.1.4.1.34380.1.2.2' => 'more CSR specific info',
       })
+      expect(trusted.hostname).to eq('cert name')
+      expect(trusted.domainname).to be_nil
     end
 
     it "is remote but lacks certificate information when it is authenticated" do
@@ -62,6 +64,8 @@ describe Puppet::Context::TrustedInformation do
       expect(trusted.authenticated).to eq('local')
       expect(trusted.certname).to eq('cert name')
       expect(trusted.extensions).to eq({})
+      expect(trusted.hostname).to eq('cert name')
+      expect(trusted.domainname).to be_nil
     end
 
     it "is authenticated local with no clientcert when there is no node" do
@@ -70,6 +74,8 @@ describe Puppet::Context::TrustedInformation do
       expect(trusted.authenticated).to eq('local')
       expect(trusted.certname).to be_nil
       expect(trusted.extensions).to eq({})
+      expect(trusted.hostname).to be_nil
+      expect(trusted.domainname).to be_nil
     end
   end
 
@@ -82,7 +88,24 @@ describe Puppet::Context::TrustedInformation do
       'extensions' => {
         '1.3.6.1.4.1.34380.1.2.1' => 'CSR specific info',
         '1.3.6.1.4.1.34380.1.2.2' => 'more CSR specific info',
-      }
+      },
+      'hostname' => 'cert name',
+      'domainname' => nil
+    })
+  end
+
+  it "extracts domainname and hostname from certname" do
+    trusted = Puppet::Context::TrustedInformation.remote(true, 'hostname.domainname.long', cert)
+
+    expect(trusted.to_h).to eq({
+      'authenticated' => 'remote',
+      'certname' => 'hostname.domainname.long',
+      'extensions' => {
+        '1.3.6.1.4.1.34380.1.2.1' => 'CSR specific info',
+        '1.3.6.1.4.1.34380.1.2.2' => 'more CSR specific info',
+      },
+      'hostname' => 'hostname',
+      'domainname' => 'domainname.long'
     })
   end
 
